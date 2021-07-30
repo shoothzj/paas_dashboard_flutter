a49929076b9c70125a06de91d55362e00c3be3af
fu_turer <futurer@outlook.com>
2021-07-30 17:39:48 +0800

    manager topic subscription

--- lib/api/pulsar/pulsar_topic_api.dart
+++ lib/api/pulsar/pulsar_topic_api.dart
@@ -51,4 +51,34 @@ class PulsarTopicAPi {
     List jsonResponse = json.decode(response.body) as List;
     return jsonResponse.map((name) => new TopicResp.fromJson(name)).toList();
   }
+
+  static Future<List<SubscriptionResp>> getSubscription(String host, int port,
+      String tenant, String namespace, String topic) async {
+    var url =
+        'http://$host:${port
+        .toString()}/admin/v2/persistent/$tenant/$namespace/$topic/subscriptions';
+    final response = await http.get(Uri.parse(url));
+    if (HttpUtil.abnormal(response.statusCode)) {
+      log('ErrorCode is ${response.statusCode}, body is ${response.body}');
+      throw Exception(
+          'ErrorCode is ${response.statusCode}, body is ${response.body}');
+    }
+    List jsonResponse = json.decode(response.body) as List;
+    return jsonResponse.map((name) => new SubscriptionResp.fromJson(name))
+        .toList();
+  }
+
+  static Future<String> deleteSubscription(String host, int port,
+      String tenant, String namespace, String topic, String subscription) async {
+    var url =
+        'http://$host:${port
+        .toString()}/admin/v2/persistent/$tenant/$namespace/$topic/subscription/$subscription/skip_all';
+    final response = await http.post(Uri.parse(url));
+    if (HttpUtil.abnormal(response.statusCode)) {
+      log('ErrorCode is ${response.statusCode}, body is ${response.body}');
+      throw Exception(
+          'ErrorCode is ${response.statusCode}, body is ${response.body}');
+    }
+    return response.body;
+  }
 }
--- lib/ui/pulsar/screen/pulsar_partitioned_topic.dart
+++ lib/ui/pulsar/screen/pulsar_partitioned_topic.dart
@@ -1,6 +1,8 @@
-
 import 'package:flutter/material.dart';
+import 'package:paas_dashboard_flutter/api/pulsar/pulsar_topic_api.dart';
 import 'package:paas_dashboard_flutter/module/pulsar/pulsar_topic.dart';
+import 'package:paas_dashboard_flutter/ui/util/alert_util.dart';
+import 'package:paas_dashboard_flutter/ui/util/spinner_util.dart';
 
 class PulsarPartitionedTopicScreen extends StatefulWidget {
   final TopicPageContext topicPageContext;
@@ -13,23 +15,105 @@ class PulsarPartitionedTopicScreen extends StatefulWidget {
   }
 }
 
-class PulsarPartitionedTopicScreenState extends State<PulsarPartitionedTopicScreen> {
+class PulsarPartitionedTopicScreenState
+    extends State<PulsarPartitionedTopicScreen> {
   final TopicPageContext topicPageContext;
 
   PulsarPartitionedTopicScreenState(this.topicPageContext);
 
+  late Future<List<SubscriptionResp>> _func;
+
   @override
   void initState() {
+    loadData();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
+    var topicsFuture = FutureBuilder(
+        future: _func,
+        builder: (ctx, snapshot) {
+          if (snapshot.hasData) {
+            List<SubscriptionResp> data =
+                snapshot.data as List<SubscriptionResp>;
+            return SingleChildScrollView(
+              child: DataTable(
+                  showCheckboxColumn: false,
+                  columns: [
+                    DataColumn(label: Text('Subscription Name')),
+                    DataColumn(label: Text('Delete Subscription')),
+                    DataColumn(label: Text('Stats')),
+                  ],
+                  rows: data
+                      .map((data) => DataRow(cells: [
+                            DataCell(
+                              Text(data.subscriptionName),
+                            ),
+                            DataCell(TextButton(
+                              child: Text('clear-backlog'),
+                              onPressed: () {
+                                PulsarTopicAPi.deleteSubscription(
+                                    topicPageContext.host,
+                                    topicPageContext.port,
+                                    topicPageContext.tenantName,
+                                    topicPageContext.namespaceName,
+                                    topicPageContext.topicName,
+                                    data.subscriptionName);
+                              },
+                            )),
+                            DataCell(TextButton(
+                              child: Text('backlog'),
+                              onPressed: () {},
+                            )),
+                          ]))
+                      .toList()),
+            );
+          } else if (snapshot.hasError) {
+            return AlertUtil.create(snapshot.error, context);
+          }
+          // By default, show a loading spinner.
+          return SpinnerUtil.create();
+        });
+    var refreshButton = TextButton(
+        onPressed: () {
+          setState(() {
+            loadData();
+          });
+        },
+        child: Text('Refresh'));
+    var body = ListView(
+      children: <Widget>[
+        Container(
+          height: 50,
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            shrinkWrap: true,
+            children: [refreshButton],
+          ),
+        ),
+        Text(
+          'Partitioned Topics',
+          style: TextStyle(fontSize: 22),
+        ),
+        topicsFuture
+      ],
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Pulsar Partitioned Topic'),
       ),
-      body: Text('Topic'),
+      body: body,
     );
+  }
+
+  void loadData() {
+    _func = PulsarTopicAPi.getSubscription(
+        topicPageContext.host,
+        topicPageContext.port,
+        topicPageContext.tenantName,
+        topicPageContext.namespaceName,
+        topicPageContext.topicName);
   }
 }
