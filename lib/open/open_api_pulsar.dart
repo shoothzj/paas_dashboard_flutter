acdaba0ab5457d784b4ee0ab988413b19f5def9d
goflutterjava <95280282+goflutterjava@users.noreply.github.com>
2022-01-13 10:47:48 +0800

    Use github flutter actions v2 (#32)

--- lib/open/open_api_pulsar.dart
+++ lib/open/open_api_pulsar.dart
@@ -1,8 +1,8 @@
 import 'package:paas_dashboard_flutter/api/pulsar/pulsar_partitioned_topic_api.dart';
 import 'package:paas_dashboard_flutter/persistent/persistent.dart';
 
-class PulsarService {
-  Future<int> partitionTopicNumber(String name, String tenant, String namespace) async {
+class OpenApiPulsar {
+  Future<int> partitionTopicCount(String name, String tenant, String namespace) async {
     var pulsarInstance = await Persistent.pulsarInstance(name);
     if (pulsarInstance == null) {
       throw ArgumentError("pulsar instance not exist");
