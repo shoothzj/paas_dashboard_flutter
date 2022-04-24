7994a8395060b78971d99f9de28a96091043c709
LeKe <95280282+goflutterjava@users.noreply.github.com>
2022-04-24 20:14:32 +0800

    Update dart eval to latest (#74)

--- lib/eval/eval_service.dart
+++ lib/eval/eval_service.dart
@@ -17,15 +17,6 @@
 // under the License.
 //
 
-import 'package:dart_eval/dart_eval.dart';
-import 'package:paas_dashboard_flutter/open/open_api_debug_sync_eval_help.dart';
-
 class EvalService {
-  static void eval(String piece) {
-    var parser = Parse();
-    parser.define(EvalOpenApiSyncDebug.declaration);
-    final code = "void main() async {" + "\n" + "final openApiSyncDebug = OpenApiSyncDebug();\n" + piece + "\n" + "}";
-    final func = parser.parse(code);
-    func('main', []);
-  }
+  static void evalPiece(String piece) {}
 }
--- test/eval/eval_service_test_debug_async_helloworld.dart
+++ test/eval/eval_service_test_debug_async_helloworld.dart
@@ -2,7 +2,7 @@ import 'package:paas_dashboard_flutter/eval/eval_service.dart';
 
 void main() async {
   EvalService.evalPiece('''
-      print(openApiSyncDebug.helloWorld());
-      print(openApiSyncDebug.echo("Hi!"));
+      print(openApiAsyncDebug.helloWorld());
+      print(openApiAsyncDebug.echo("Hi!"));
     ''');
 }
