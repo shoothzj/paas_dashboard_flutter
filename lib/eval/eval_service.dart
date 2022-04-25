a17f402bdda91b513232439c1d83519a07e701a4
LeKe <95280282+goflutterjava@users.noreply.github.com>
2022-04-25 09:35:24 +0800

    refract open api class name (#75)

--- lib/eval/eval_service.dart
+++ lib/eval/eval_service.dart
@@ -17,6 +17,29 @@
 // under the License.
 //
 
+import 'package:dart_eval/dart_eval.dart';
+import 'package:paas_dashboard_flutter/open/open_api_debug_async_bridge.dart';
+import 'package:paas_dashboard_flutter/open/open_api_debug_sync_bridge.dart';
+
 class EvalService {
-  static void evalPiece(String piece) {}
+  static void evalPiece(String piece) {
+    var compiler = Compiler();
+    compiler.defineBridgeClasses([OpenApiDebugSyncBridge.$declaration]);
+    compiler.defineBridgeClasses([OpenApiDebugAsyncBridge.$declaration]);
+    final code = "" +
+        "import 'package:paas_dashboard_flutter/open/open_api_debug_async.dart';\n" +
+        "import 'package:paas_dashboard_flutter/open/open_api_debug_sync.dart';\n" +
+        "void main() async {\n" +
+        "final openApiDebugSync = OpenApiDebugSync();\n" +
+        "final openApiDebugAsync = OpenApiDebugAsync();\n" +
+        piece +
+        "\n" +
+        "}";
+    final program = compiler.compile({
+      'package:example': {'main.dart': code}
+    });
+    final runtime = Runtime.ofProgram(program);
+    runtime.setup();
+    runtime.executeNamed(0, 'main');
+  }
 }
